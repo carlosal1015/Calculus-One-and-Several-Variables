a0d6692807ac6cc34d6c619eec21bae6679d28d1
Oromion <caznaranl@uni.pe>
2017-09-10 23:12:22 -0500

    Create ifelse.cpp

---
+++ Integral calculus/Cpp/ifelse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int number1 = 15;
+int number2 = 14;
+
+bool booleana = true;
+int main ()
+{
+  if(number1<number2)
+  {
+      cout << "Es menor" << endl;
+  }
+  else
+  {
+      cout << "No es menor" << endl;
+  }
+  cin.get();
+  return 0;
+}
