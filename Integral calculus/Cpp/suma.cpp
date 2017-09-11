5ee8e0cfbd7c0e2aceb74410314b342b75f3be4d
Oromion <caznaranl@uni.pe>
2017-09-10 22:19:27 -0500

    Create suma.cpp

---
+++ Integral calculus/Cpp/suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdlib.h>
+int num = 34;
+int num2 = 5;
+suma = num + num2;
+using namespace std;
+int main ()
+{
+cout << suma "\n\n" << endl;
+system ("PAUSE");
+return 0;
+}
