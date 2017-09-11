e435cf8927504f0dbc57ec14e0d18bc700837298
Oromion <caznaranl@uni.pe>
2017-09-11 00:15:19 -0500

    Create array.cpp

---
+++ Integral calculus/Cpp/array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    char c[4]
+    c[0] = 'H';
+    c[1] = 'o';
+    c[2] = 'l';
+    c[3] = 'a';
+    
+    
+    for (int i=0; i<4; i++)
+    cin.get;
+    return 0;
+}
