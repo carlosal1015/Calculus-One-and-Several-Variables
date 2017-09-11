3d05b9c2d535320d7408a0618ba8581e06596581
Oromion <caznaranl@uni.pe>
2017-09-10 23:15:18 -0500

    Update elseif.cpp

--- Integral calculus/Cpp/elseif.cpp
+++ Integral calculus/Cpp/elseif.cpp
@@ -1,1 +1,25 @@
-d
+#include<iostream>
+
+using namespace std;
+
+int number1 = 14;
+int number2 = 14;
+
+bool booleana = true;
+int main ()
+{
+  if(number1<number2)
+  {
+      cout << "Es menor" << endl;
+  }
+  else if (number1 == number2)
+  {
+      cout << "Valen lo mismo" << endl;
+  }
+  else
+  {
+      cout << "No es menor" << endl;
+  }
+  cin.get();
+  return 0;
+}
