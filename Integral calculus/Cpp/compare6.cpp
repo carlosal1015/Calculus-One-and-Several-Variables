c0bbca357bbeeaac27655ca1e90b686636ccb835
Oromion <caznaranl@uni.pe>
2017-09-10 23:07:09 -0500

    Create compare6.cpp

---
+++ Integral calculus/Cpp/compare6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+bool compare;
+
+int number1, number2, number3, number4;
+
+int main ()
+{
+  number1 = 12;
+  number2 = 20;
+  number3 = 4;
+  number4 = 13;
+  compare = !(number4 > number1);
+  cout << compare << endl;
+  cin.get();
+  return 0;
+}
