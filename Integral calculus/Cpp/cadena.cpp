cd55f909ede61a124666346c990e1242de378e13
Oromion <caznaranl@uni.pe>
2017-09-10 22:26:30 -0500

    Create cadena.cpp

---
+++ Integral calculus/Cpp/cadena.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+char* cadena= "ApplicatedMath";
+
+int main ()
+{
+  cout << cadena << "\n \n" << endl;
+  system("PAUSE");
+  return 0;
+}
