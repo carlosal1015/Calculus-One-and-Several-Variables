d61ebcacd385f160f198c17b327c7c263f6ec7d1
Oromion <caznaranl@uni.pe>
2017-09-10 23:10:57 -0500

    Create if.cpp

---
+++ Integral calculus/Cpp/if.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int number1 = 12;
+int number2 = 14;
+
+bool booleana = true;
+int main ()
+{
+  if(number1<number2)
+  {
+      cout << "Es menor" << endl;
+  }
+  cin.get();
+  return 0;
+}
