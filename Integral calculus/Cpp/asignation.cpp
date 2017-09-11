a85a4c1caea639d98d7beb5c407c1110aee28e70
Oromion <caznaranl@uni.pe>
2017-09-10 22:52:31 -0500

    Create asignation.cpp

---
+++ Integral calculus/Cpp/asignation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int asignation;
+
+int main ()
+{
+  asignation = 12;
+  asignation *= 3;
+  cout << asignation << endl;
+  getch();
+  return 0;
+}
