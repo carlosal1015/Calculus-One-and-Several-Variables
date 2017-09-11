2b1f6ec9f1442213f766127158faa251bd9d7ffd
Oromion <caznaranl@uni.pe>
2017-09-11 00:10:26 -0500

    Create dowhile.cpp

---
+++ Integral calculus/Cpp/dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+  int i = 10;
+  do // Ejecuta y luego cumple el código.
+  {
+      cout << i << endl;
+      i++;
+  }while(i <= 5)
+  cin.get();
+  return 0;
+}
