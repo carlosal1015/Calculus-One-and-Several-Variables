1be473837afac0b4149542cd560f32ccd89353e5
Oromion <caznaranl@uni.pe>
2017-09-10 22:40:32 -0500

    Create double.cpp

---
+++ Integral calculus/Cpp/double.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main ()
+{
+  double number= 0.00000000000000000065064064604;
+  cout << number << endl;
+  getch();
+  return 0;
+}
