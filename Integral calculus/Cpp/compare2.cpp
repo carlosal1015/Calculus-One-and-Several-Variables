cd141321f900fdcbd97788be9d8692fed31bf8ab
Oromion <caznaranl@uni.pe>
2017-09-10 22:57:48 -0500

    Create compare2.cpp

---
+++ Integral calculus/Cpp/compare2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+bool compare;
+
+int number1 = 11;
+int number2 = 12;
+
+int main ()
+{
+  compare = (number1 != number2);
+  cout << compare << endl;
+  getch();
+  return 0;
+}
