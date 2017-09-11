5f9813288eb1b133853bda186daecccb87ccf26f
Oromion <caznaranl@uni.pe>
2017-09-10 22:35:17 -0500

    Update float.cpp

--- Integral calculus/Cpp/float.cpp
+++ Integral calculus/Cpp/float.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<conio.h>
-
+using namespace std;
 int main ()
 {
-  float number = 2.567
-  cout << number << endl;
+  float number = 2.567155;
+  float number2 = 1.3;
+  cout << number + number2 << endl;
   getch();
   return 0;
 }
