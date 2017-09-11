201d78c69496c84fbde1a8a27b1144d4cf375ccc
Oromion <caznaranl@uni.pe>
2017-09-10 23:22:14 -0500

    Create switch.cpp

---
+++ Integral calculus/Cpp/switch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    char c;
+    cout << "Escribe una letra" << endl;
+    cin >> c;
+    else if (c == e)
+    switch(c)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout << "Es una vocal \n" << endl;
+    }
+    system("pause");
+    return 0;
+}
