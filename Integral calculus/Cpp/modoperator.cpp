c776f018aba9f6af3baf1f623ec71e957245fef6
Oromion <caznaranl@uni.pe>
2017-09-10 22:50:18 -0500

    Create modoperator.cpp

---
+++ Integral calculus/Cpp/modoperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int number1 = 5;
+int number2 = 2;
+
+int operation;
+
+int main ()
+{
+  operation = number1 % number2;
+  cout << operation << endl;
+  getch();
+  return 0;
+}
