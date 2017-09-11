07bb349fd64e279d951160bfe8b3b54c20f82894
Oromion <caznaranl@uni.pe>
2017-09-11 00:08:45 -0500

    Update while.cpp

--- Integral calculus/Cpp/while.cpp
+++ Integral calculus/Cpp/while.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+
 using namespace std;
+
 int main ()
 {
   int i = 0;
-  while(i <= 5)
+  while(i <= 5)//cumple el código y luego ejecuta
   {
       cout << i << endl;
       i++;
